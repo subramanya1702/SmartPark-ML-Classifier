5a03a79998a0f300a86d179fba50eb5ae7022da1
Subramanya1702 <keshavas@oregonstate.edu>
2023-05-22 01:43:38 -0700

    *Clean up local image directories | *Add a validation in lambda function to check if all the required fields are present in th dybamodb response

--- smart_park/my_utils/preprocess3.py
+++ smart_park/my_utils/preprocess3.py
@@ -5,20 +5,25 @@ import cv2
 import numpy as np
 
 
-def pre_process(fetched_image, preprocess_image_dir_path, filename):
+def pre_process(fetched_image, original_image_dir_path, preprocess_image_dir_path, filename):
     config_obj = configparser.ConfigParser()
     config_obj.read("config/config.ini")
     PREPROCESSING_CONFIG = config_obj["PRE_PROCESSING_BOUNDS"]
-
-    downscaled_image = cv2.cvtColor(fetched_image, cv2.COLOR_BGR2GRAY)
 
     x_coordinate = PREPROCESSING_CONFIG["x-coordinate"]
     y_coordinate = PREPROCESSING_CONFIG["y-coordinate"]
     height = PREPROCESSING_CONFIG["height"]
     width = PREPROCESSING_CONFIG["width"]
 
+    # Convert the original image to gray scale and crop it later
+    downscaled_image = cv2.cvtColor(fetched_image, cv2.COLOR_BGR2GRAY)
     pre_processed_image = downscaled_image[int(y_coordinate): int(y_coordinate) + int(height),
                           int(x_coordinate): int(x_coordinate) + int(width)]
+
+    # Crop the original image and save it under pre_processed_image directory
+    fetched_image = fetched_image[int(y_coordinate): int(y_coordinate) + int(height),
+                    int(x_coordinate): int(x_coordinate) + int(width)]
+    cv2.imwrite(os.path.join(original_image_dir_path, filename), fetched_image)
 
     pts = np.array(
         [[0, 73], [208, 71], [138, 188], [260, 190], [359, 194], [453, 197], [547, 202], [645, 206], [739, 206],
@@ -39,12 +44,10 @@ def pre_process(fetched_image, preprocess_image_dir_path, filename):
          [0, 63]], np.int32)
     pts3 = pts3.reshape((-1, 1, 2))
 
-    poly_image = cv2.fillPoly(pre_processed_image, [pts3], (255, 255, 255))
+    cv2.fillPoly(pre_processed_image, [pts3], (255, 255, 255))
 
-    # image = cv2.polylines(img, [pts],
-    #                       isClosed, color, thickness)
-    poly_image = cv2.fillPoly(pre_processed_image, [pts2], (255, 255, 255))
+    cv2.fillPoly(pre_processed_image, [pts2], (255, 255, 255))
     poly_image = cv2.fillPoly(pre_processed_image, [pts], (255, 255, 255))
-    cv2.imwrite(os.path.join(preprocess_image_dir_path, filename), poly_image)
+    cv2.imwrite(os.path.join(preprocess_image_dir_path, filename), pre_processed_image)
     print(f"the preprocessed image is stored at {preprocess_image_dir_path}")
     return poly_image
